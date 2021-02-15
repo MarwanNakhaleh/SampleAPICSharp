be93b9422a4cd2acc0a4a01ec27c70b87c3ece6c
Marwan Nakhaleh <mnakhaleh@gmail.com>
2021-02-15 12:03:52 -0500

    Logging and cleanup (#1)
    * logging and cleanup
    
    * sealed

--- SampleAPI/Data/Queries/Query.cs
+++ SampleAPI/Data/Queries/Query.cs
@@ -1,21 +1,20 @@
-﻿using SampleAPI.Data.Repositories;
-using SampleAPI.Domain;
+﻿using Microsoft.Extensions.Logging;
+using SampleAPI.Data.Repositories;
 using SampleAPI.Models;
-using System.Collections.Generic;
 
 namespace SampleAPI.Data.Queries
 {
-    public class Query : BaseQuery
+    public sealed class Query : BaseQuery
     {
-        private readonly BaseRepository _repository;
-
-        public Query(BaseRepository repository)
+        public Query(BaseRepository repository, ILoggerFactory logger) : base(repository, logger)
         {
             _repository = repository;
+            _logger = logger.CreateLogger("Query");
         }
 
         public override CourseModel GetCourseInformation()
         {
+            _logger.LogInformation("retrieving course data for client");
             return new CourseModel(_repository.GetCourses());
         }
     }
